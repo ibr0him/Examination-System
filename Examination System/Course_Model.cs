1d4d4745bfcdc81b3e14789393a53ea99442d562
Yahya-Tarek <yahya.tmohiee@gmail.com>
2025-02-05 20:35:39 +0200

    instr validation

---
+++ Examination System/Course_Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examination_System
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int StudentCount { get; set; }
+
+        public Course(string name, int id, int studentCount)
+        {
+            Name = name;
+            Id = id;
+            StudentCount = studentCount;
+        }
+    }
+
+}
